972e3eb1b85b192fd6938c59cc09ea72f67557ac
Leo Heidelberger <leo.heidelberger@radius.polytechnique.fr>
2019-11-11 18:11:00 +0100

    performance improvements

--- simulator.py
+++ simulator.py
@@ -2,7 +2,8 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-import tensorflow.keras as keras
+print("GPUs found :", tf.config.experimental.list_physical_devices("GPU"))
+
 
 gi = np.array([[0,-1,0],[0,0,0],[0,+1,0]])/2
 gj = np.array([[0,0,0],[-1,0,1],[0,0,0]])/2
@@ -27,20 +28,17 @@ lap_vec_kernel = tf.constant(lap_vec_kernel)
 
 def grad(u) :
     expended = tf.expand_dims(tf.expand_dims(u, 0), -1)
-    #print(expended.shape)
     return tf.nn.convolution(expended, grad_kernel, padding="SAME", strides=1)[0]
 def lap(u) :
     expended = tf.expand_dims(tf.expand_dims(u, 0), -1)
-    #print(expended.shape)
     return tf.nn.convolution(expended, lap_kernel, padding="SAME", strides=1)[0,:,:,0]
 def lap_vec(u) :
     expended = tf.expand_dims(u, 0)
-    #print(expended.shape)
     return tf.nn.convolution(expended, lap_vec_kernel, padding="SAME", strides=1)[0,:,:]
 def div(u) :
     expended = tf.expand_dims(u, 0)
-    #print(expended.shape)
     return tf.nn.convolution(expended, div_kernel, padding="SAME", strides=1)[0,:,:,0]
+
 
 def take(a,indices_i, indices_j) :
     m,n = a.shape[:2]
@@ -49,6 +47,7 @@ def take(a,indices_i, indices_j) :
     res = np.take(reshaped,full_idices, axis=0)
     return res.reshape(m,n,2)
 
+#@tf.function
 def solve_step_diff(u, dt, dx, nu, v, lr) :
     with tf.GradientTape() as tape :
         left = v - dt*nu*lap_vec(v)
@@ -62,44 +61,26 @@ def solve_step_diff(u, dt, dx, nu, v, lr) :
     grad_l = tape.gradient(loss,[v])[0]
     return v - lr * grad_l,loss
 
-def solve_diffusion(u, dt, dx, nu) :
-    #solving (I - dt * nu * lap ) v = u
-    v = tf.Variable(np.random.normal(size=(u.shape)))
-    for k in range(100) :      
-        next_v, loss = solve_step_diff(u, dt, dx, nu, v,0.08)
-        v.assign(next_v)
-    print("loss diff",loss.numpy())
-    return v.numpy()
-
-#@tf.function
-def solve_step_poisson(u, target,w, opt):
+def solve_step_poisson_init(u, target):
     with tf.GradientTape() as tape :
-        #res = div(grad(u))
         res = lap(u)
         diff = res-target
         diff2 = diff*diff
-        grad_u = w-grad(u)
-        left_border = tf.math.reduce_sum(tf.math.square(grad_u[:,0,1]))
-        right_border = tf.math.reduce_sum(tf.math.square(grad_u[:,-1,1]))
-        top_border = tf.math.reduce_sum(tf.math.square(grad_u[0,:,0]))
-        bottom_border = tf.math.reduce_sum(tf.math.square(grad_u[-1,:,0]))
-        loss = tf.math.reduce_sum(diff2) #+10*(left_border + right_border + top_border + bottom_border)
-    opt.apply_gradients(zip(tape.gradient(loss,[u]), [u]))
-    #print(loss.numpy())
-    return loss
+        loss = tf.math.reduce_sum(diff2)
+    grad_l = tape.gradient(loss,[u])[0]
+    return -0.01*grad_l, grad_l,loss
 
-def solve_poisson(w) :
-    #solving : lap u = div w
-    target = div(w)
-    u = tf.Variable(np.random.normal(size=(w.shape[:2])))
-    opt = keras.optimizers.Adam(lr=0.6)
-    for k in range(300) :      
-        loss = solve_step_poisson(u, target, w,opt) 
-    div_w = div(w - grad(u))
-    print(np.sum(div_w*div_w))
-    print("loss poisson", loss.numpy())
-    return u.numpy()
-
+#@tf.function
+def solve_step_poisson(u, target,last_grad, s):
+    with tf.GradientTape() as tape :
+        res = lap(u)
+        diff = res-target
+        diff2 = diff*diff
+        loss = tf.math.reduce_sum(diff2)
+    grad_l = tape.gradient(loss,[u])[0]
+    diff_grad = grad_l-last_grad
+    alpha = tf.reduce_sum(diff_grad*s) / tf.reduce_sum(diff_grad*diff_grad)
+    return -alpha*grad_l, grad_l,loss
 
 class Simulator :
     def __init__(self, m, n, dx, nu, border_condition, force):
@@ -111,14 +92,18 @@ class Simulator :
         self.force = force
         self.w = w = np.zeros((m,n,2))
         self.dust = [[np.random.random()*(m-1), np.random.random()*(n-1)] for k in range(400)] 
-        
+        self.u = tf.Variable(np.random.normal(size=(self.w.shape[:2])))
+        self.indices = tf.constant(np.indices((m,n)).swapaxes(0,2).swapaxes(0,1), dtype=tf.float32)
+        self.clipping = tf.constant(np.stack([np.ones((m,n), dtype=np.int32)*m,np.ones((m,n), dtype=np.int32)*n], axis=-1))
+
     def display(self, c='b'):
         plt.scatter([d[1] for d in self.dust], [d[0] for d in self.dust], c=c, s=5)
         
     def compute_w1(self, w0, dt):
         return w0 + dt*self.force(self.m,self.n, w0)
         
-    def compute_w2(self, w1, dt):
+    def compute_w2_numpy(self, w1, dt):
+        #old numpy deprecated version
         #Position of point i,j at t-dt, shape : m,n,2 
         m,n = self.m,self.n
         indices = np.indices((m,n)).swapaxes(0,2).swapaxes(0,1) - dt*w1
@@ -138,20 +123,56 @@ class Simulator :
                 w2 += np.reshape(frac[i][:,:,0]*frac[j][:,:,1], (m,n,1)) * take(w1, indices[i][:,:,0], indices[j][:,:,1])
         return w2
     
+    def compute_w2(self, w1, dt):
+        #Position of point i,j at t-dt, shape : m,n,2 
+        w1 = tf.constant(w1, dtype=tf.float32)
+        m,n = self.m,self.n
+        indices = self.indices - dt*w1
+        indices_floor = tf.math.floor(indices)
+        frac = indices-indices_floor
+        frac = [1-frac,frac]
+        indices_floor = tf.dtypes.cast(indices_floor, tf.int32)
+        indices_ceil = indices_floor + 1
+        indices_ceil = tf.clip_by_value(indices_ceil, 0, self.clipping)
+        indices_floor = tf.clip_by_value(indices_floor, 0, self.clipping)  
+        indices = [indices_floor, indices_ceil] # shape : 2 m,n,2
+        w2 = tf.zeros((m,n,2))
+        for i in range(2) :
+            for j in range(2) :
+                indices_to_take = tf.stack([indices[i][:,:,0], indices[j][:,:,1]], axis=-1)
+                w2 = w2 + tf.expand_dims(frac[i][:,:,0] * frac[j][:,:,1], -1) * tf.gather_nd(w1,indices_to_take)
+        return w2.numpy().astype(np.float64)
+    
     
     def compute_w3(self, w2, dt):
-        return solve_diffusion(w2, dt, self.dx, self.nu)
+        #solving (I - dt * nu * lap ) v = w2
+        v = tf.Variable(w2.copy())
+        for k in range(100) :      
+            next_v, loss = solve_step_diff(w2, dt, self.dx, self.nu, v,0.08)
+            v.assign(next_v)
+            if loss/(self.m*self.n) < 0.00001 :
+                #print(k)
+                break
+        #print("loss diff",loss.numpy()/(self.m*self.n))
+        return v.numpy()
 
 
     def compute_w4(self, w3, dt):
-        global test_u
-        u = solve_poisson(w3)
-        test_u = u
-
-        grad_u = grad(u).numpy()
-        w4 = w3 - grad_u
+        target = div(w3)
+        self.u.assign(np.random.normal(size=(w3.shape[:2])))
+        s, last_grad, loss = solve_step_poisson_init(self.u, target)
+        self.u.assign(self.u+s)
+        for k in range(100) :      
+            s, last_grad, loss = solve_step_poisson(self.u, target,last_grad,s)
+            self.u.assign(self.u+s)
+            #print(loss.numpy())
+            if loss/(self.m*self.n) < 0.001 : 
+                #print(k)
+                break
+        #print("poisson :",k, loss.numpy()/(self.m*self.n))
         
-        return w4
+        grad_u = grad(self.u).numpy()
+        return  w3 - grad_u
     
     def update_dust(self, dt) :
         for d in self.dust :
@@ -171,6 +192,7 @@ class Simulator :
         w2 = self.compute_w2(w1, dt)
         self.border_condition(w2)
 
+        cached_u = self.u.numpy().copy()
         w3 = self.compute_w3(w2, dt)
         self.border_condition(w3)
 
@@ -178,3 +200,13 @@ class Simulator :
         self.border_condition(w4)
         self.w = w4
         self.update_dust(dt)
+        return w3, cached_u
+    
+def display_w(w) :
+    m,n = w.shape[:2]
+    for k in range(20) :
+        for l in range(20) :
+            i = (m-2)*k/20 + 1
+            j = (n-2)*l/20 + 1
+            plt.plot([j, j+w[int(i), int(j), 1]*3], [i, i+w[int(i), int(j), 0]*3], c="b")
+            plt.scatter(j,i, c="b", s=5)
